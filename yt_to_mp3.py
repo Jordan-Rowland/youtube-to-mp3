b870bbd21653e3497739e019dda727c63420af39
Jordan <36084892+Jordan-Rowland@users.noreply.github.com>
2019-11-23 15:56:32 -0800

    adds file renaming to end of script

--- yt_to_mp3.py
+++ yt_to_mp3.py
@@ -1,10 +1,12 @@
 import os
+import re
 import subprocess
 
 from pytube import YouTube
 
 
 videos = """
+
 """.split()
 
 for index, video in enumerate(videos):
@@ -14,13 +16,11 @@ for index, video in enumerate(videos):
         print(yt.title)
         stream = yt.streams.filter(only_audio=True).filter(subtype="mp4").first()
         print(stream)
-
-        # Uncomment this to download
         print("Downloading video...")
         stream.download()
     except Exception:
         print("Could not get video :(")
-        cont
+        continue
     bash_command_1 = subprocess.Popen(
         ["ffmpeg", "-i", f"{yt.title}.mp4", "-vn", "-f", "wav", "-"],
         stdout=subprocess.PIPE,
@@ -33,3 +33,22 @@ for index, video in enumerate(videos):
     output = bash_command_2.communicate()[0]
     print("Deleting original video file...")
     os.remove(f"{yt.title}.mp4")
+
+
+mp3s = [x for x in os.listdir() if x.endswith(".mp3")]
+
+title_match = re.compile("([\w+\s+]+)\s-\s([\w+\s+]+)")
+
+for file in mp3s:
+    match = re.search(title_match, file)
+    new_title = (
+        f"{match.groups()[0].strip()} - " \
+        f"{match.groups()[1].strip()}.mp3"
+        )
+    if file != new_title:
+        print(f"Renaming file {file} to:\n" \
+              f"{new_title}"
+        )
+        os.rename(file, new_title)
+        # os.rename(new_title, file)
+
