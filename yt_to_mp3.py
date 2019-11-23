4a3b4a7aa09eec5882e0832f5a6fa87432a0e2d2
Jordan <36084892+Jordan-Rowland@users.noreply.github.com>
2019-11-23 01:00:10 -0800

    adds try - except blocks

--- yt_to_mp3.py
+++ yt_to_mp3.py
@@ -3,20 +3,24 @@ import subprocess
 
 from pytube import YouTube
 
-videos = [
-    "https://www.youtube.com/watch?v=0ok0glLJsr4"
-]
 
+videos = """
+""".split()
 
 for index, video in enumerate(videos):
-    print(f"Getting {index + 1} of {len(videos)}...")
-    yt = YouTube(video)
-    print(yt.title)
-    stream = yt.streams.filter(only_audio=True).filter(subtype="mp4").first()
-    print(stream)
+    print(f"{index + 1} of {len(videos)}")
+    try:
+        yt = YouTube(video)
+        print(yt.title)
+        stream = yt.streams.filter(only_audio=True).filter(subtype="mp4").first()
+        print(stream)
 
-    # Uncomment this to download
-    stream.download()
+        # Uncomment this to download
+        print("Downloading video...")
+        stream.download()
+    except Exception:
+        print("Could not get video :(")
+        cont
     bash_command_1 = subprocess.Popen(
         ["ffmpeg", "-i", f"{yt.title}.mp4", "-vn", "-f", "wav", "-"],
         stdout=subprocess.PIPE,
@@ -25,5 +29,7 @@ for index, video in enumerate(videos):
         ["lame", "-V", "3", "-", f"{yt.title}.mp3"], stdin=bash_command_1.stdout
     )
     bash_command_1.stdout.close()
+    print("Converting to MP3...")
     output = bash_command_2.communicate()[0]
+    print("Deleting original video file...")
     os.remove(f"{yt.title}.mp4")
